d0b3b7b480097545f57b8286f0a80123bbd2f8b1
Saratvk <saratavakoli77@ut.ac.ir>
2019-06-05 00:06:16 +0430

    cache controller changed

--- cacheController.v
+++ cacheController.v
@@ -1,24 +1,17 @@
-module cpu ( input clk, rst, cache_ready, /*input [31:0] read_data*/ output reg cache_read, cache_write, output reg [14:0] address /* , output reg [31:0] write_data*/ );
+module cpu ( input clk, rst, cache_ready, output reg cache_read, cache_write, output reg [14:0] address);
 	reg [13:0] successful_access;
-
-	always@(posedge clk, posedge rst)
-	begin
-		if (rst)
-		begin
-			address       <= 15'd1024;
+	always@(posedge clk, posedge rst) begin
+		if (rst) begin
+			address <= 15'd1024;
 			successful_access <= 14'b0;
-			//write_data    <= 32'b0;
 		end
 		else begin
-			cache_read  <= 1'b0;
 			cache_write <= 1'b0;
 			cache_read  <= (successful_access < 14'd8192) ? 1'b1 : 1'b0;
-
 			if (successful_access < 14'd8192 && cache_ready) begin	
-				address       <= address + 15'd1;
+				address <= address + 15'd1;
 				successful_access <= successful_access + 14'd1;
 			end
 		end
 	end
-
 endmodule
